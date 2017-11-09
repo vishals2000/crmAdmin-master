a3799f67cf20c9e73d8cee418ec06daa75c6d393
Vishal Babu Meriga <vishals2000@gmail.com>
2017-11-09 20:22:00 +0530

    Insights developments

--- src/main/webapp/app/dashboard/linechart/linechart.component.ts
+++ src/main/webapp/app/dashboard/linechart/linechart.component.ts
@@ -7,11 +7,17 @@ import { Message } from 'primeng/components/common/api';
     templateUrl: './linechart.component.html',
     styles: []
 })
+
 export class LinechartComponent implements OnInit {
     data: any;
     msgs: Message[];
+    chartHeading: string;
+    myClass = [];
 
     constructor() {
+        this.chartHeading = 'Messages';
+        this.addClass('active');
+        this.removeClass('active');
         this.data = {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
@@ -32,6 +38,106 @@ export class LinechartComponent implements OnInit {
     }
 
     ngOnInit() {
+    }
+
+    addClass(clas) {
+        this.myClass.push(clas);
+      }
+
+      removeClass(clas) {
+        const i = this.myClass.indexOf(clas);
+        if (i > -1)  {
+            this.myClass.splice(i, 1);
+        }
+      }
+
+      checkClass() {
+        if (this.myClass.indexOf('red') === -1) {
+           alert('false');
+        } else {
+           alert('true');
+        }
+      }
+
+    getMessages() {
+        this.chartHeading = 'Messages';
+        this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [22, 55, 88, 88, 66, 55, 44],
+                    fill: false,
+                    borderColor: '#4bc0c0'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderColor: '#565656'
+                }
+            ]
+        };
+    }
+    getTotalUsers() {
+        this.chartHeading = 'Total Users';
+        this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [32, 51, 80, 65, 68, 56, 41],
+                    fill: false,
+                    borderColor: '#4bc0c0'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [21, 38, 50, 29, 56, 17, 60],
+                    fill: false,
+                    borderColor: '#565656'
+                }
+            ]
+        };
+    }
+    getUniqueUsers() {
+        this.chartHeading = 'Unique Users';
+        this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [45, 65, 78, 98, 68, 56, 41],
+                    fill: false,
+                    borderColor: '#4bc0c0'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderColor: '#565656'
+                }
+            ]
+        };
+    }
+    getSessions() {
+        this.chartHeading = 'Sessions';
+        this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'First Dataset',
+                    data: [24, 45, 28, 58, 60, 25, 84],
+                    fill: false,
+                    borderColor: '#4bc0c0'
+                },
+                {
+                    label: 'Second Dataset',
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    fill: false,
+                    borderColor: '#565656'
+                }
+            ]
+        };
     }
 
     selectData(event) {
