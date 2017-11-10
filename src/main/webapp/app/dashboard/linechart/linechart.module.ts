27444d805e96b96bf18fec702a853f00f318ce93
Vishal Babu Meriga <vishals2000@gmail.com>
2017-11-10 19:38:22 +0530

    Insights routing changes from Apps and Templates

--- src/main/webapp/app/dashboard/linechart/linechart.module.ts
+++ src/main/webapp/app/dashboard/linechart/linechart.module.ts
@@ -10,7 +10,7 @@ import {
 } from './';
 
 const DASHBOARD_STATES = [
-    linechartRoute
+    ...linechartRoute
 ];
 
 @NgModule({
--- src/main/webapp/app/dashboard/linechart/linechart.route.ts
+++ src/main/webapp/app/dashboard/linechart/linechart.route.ts
@@ -1,13 +1,33 @@
-import { Route } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes, CanActivate } from '@angular/router';
 import { UserRouteAccessService } from '../../shared';
 import { LinechartComponent } from './linechart.component';
 
-export const linechartRoute: Route = {
-    path: 'linechart',
-    component: LinechartComponent,
-    data: {
-        authorities: ['ROLE_USER'],
-        pageTitle: 'dashboard.linechart.home.title'
+export const linechartRoute: Routes = [
+    {
+        path: 'linechart',
+        component: LinechartComponent,
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'dashboard.linechart.home.title'
+        },
+        canActivate: [UserRouteAccessService]
     },
-    canActivate: [UserRouteAccessService]
-};
+    {
+        path: 'linechart/project/:id',
+        component: LinechartComponent,
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'dashboard.linechart.home.title'
+        },
+        canActivate: [UserRouteAccessService]
+    },
+    {
+        path: 'linechart/template/:id',
+        component: LinechartComponent,
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'dashboard.linechart.home.title'
+        },
+        canActivate: [UserRouteAccessService]
+    }
+];
